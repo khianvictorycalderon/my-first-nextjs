1ff8728152b401d4542021ecfc7c0999f6c38b33
khian calderon <ckhianvictory@gmail.com>
2025-07-21 14:00:13 +0800

    Added Server Side Rendering

--- app/fetch/page.tsx
+++ app/fetch/page.tsx
@@ -10,6 +10,7 @@ const FetchPage = async() => {
         next: {
             revalidate: 10 // Runs this very 10 second
         },
+        // Adding this forces this page to be rendered on the server side
         // cache: "no-store" // Doesn't store data in the browser
     });
     const users: UserProps[] = await res.json();
@@ -17,6 +18,7 @@ const FetchPage = async() => {
     return (
         <>
             <h1>Fetched Users:</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
             <ul>
                 {users.map(user => (
                     <li key={user.id}>
